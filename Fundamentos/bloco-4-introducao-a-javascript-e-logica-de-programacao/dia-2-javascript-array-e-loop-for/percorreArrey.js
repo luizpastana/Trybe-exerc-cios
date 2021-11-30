be56c5115ca166e0e87750f6bbb6a2fa30e2cbe9
luizpastana <lcsp84@gmail.com>
2021-11-29 22:57:53 -0300

    inclusao do arquivo media

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/percorreArrey.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/percorreArrey.js
@@ -3,9 +3,3 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for(number of numbers){
     console.log(number);
 }
-
-let count = 0;
-for (const iterator of numbers) {
-    count += iterator;
-}
-console.log(count);
