11ff0dd57dc37202257337cda229ef8db19cb0de
luizpastana <lcsp84@gmail.com>
2021-11-30 06:42:05 -0300

    inclusao da menssagem maior ou menor que vinte

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/media.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/media.js
@@ -1,8 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+let media = 0;
 let soma = 0;
 
 for (const iter of numbers) {
     soma += iter;
 }
-console.log(soma/numbers.length);
+media = soma/numbers.length;
+
+if(media > 20){
+    console.log('Valor maior que vinte.');
+}else{
+    console.log('Valor menor que vinte.');
+}
+
+console.log(media);
