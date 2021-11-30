afd41040392499fedeba2bdd8ab210e1de3f23c8
luizpastana <lcsp84@gmail.com>
2021-11-30 19:36:49 -0300

    Desenha quadrado de asterisco

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/asterisco.js
@@ -0,0 +1,10 @@
+let n = 5;
+let asterisco = '*';
+let linha = '';
+
+for (let index = 0; index < n; index += 1) {
+    linha += asterisco; 
+}
+for (index = 0; index < n; index += 1) {
+    console.log(linha);
+}
